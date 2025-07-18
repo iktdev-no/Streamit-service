00b636e703b16451415a0d9e57e669fa602f446a
Brage Skjønborg <bskjon@outlook.com>
2025-07-19 00:02:36 +0200

    Removed cors filter

--- src/main/kotlin/no/iktdev/streamit/service/stream/ContentController.kt
+++ src/main/kotlin/no/iktdev/streamit/service/stream/ContentController.kt
@@ -17,6 +17,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody
 import org.springframework.web.util.UrlPathHelper
@@ -40,6 +41,7 @@ open class ContentController {
             }
         }
     }
+
 
     @RequiresAuthentication(Mode.Strict)
     @GetMapping("video/{collection}/{video}")
