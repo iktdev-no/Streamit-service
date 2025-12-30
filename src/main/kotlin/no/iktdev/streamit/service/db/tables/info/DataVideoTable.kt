6c514d808e9320f4c576b2600c2b47b3ce049e23
Brage Skjønborg <bskjon@outlook.com>
2025-12-30 22:37:48 +0100

    Fixed usage and dependency (missing)

--- src/main/kotlin/no/iktdev/streamit/service/db/tables/info/DataVideoTable.kt
+++ src/main/kotlin/no/iktdev/streamit/service/db/tables/info/DataVideoTable.kt
@@ -3,7 +3,7 @@ package no.iktdev.streamit.service.db.tables.info
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.Column
 
-object DataVideoTable : IntIdTable(name = "MEDIA_VIDEO_DATA") {
+object DataVideoTable : IntIdTable(name = "MEDIA_DATA_VIDEO") {
     val file: Column<String> = varchar("SOURCE", 200).uniqueIndex()
     val codec: Column<String> = varchar("CODEC", 12)
     val pixelFormat: Column<String> = varchar("PIXEL_FORMAT", 12)
